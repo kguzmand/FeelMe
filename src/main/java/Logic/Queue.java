2b510e1d3d6bd7618eb2f8520363022c3d3c6f49
joalvarado27 <146464326+joalvarado27@users.noreply.github.com>
2023-10-03 17:45:22 -0500

    Queue.java

--- src/main/java/Logic/Queue.java
+++ src/main/java/Logic/Queue.java
@@ -1,14 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.otroprojecttest;
+package Logic;
 
-/**
- *
- * @author esteb
- * @param <T>
- */
 public class Queue<T> {
     private class Node<T> {
         private T data;

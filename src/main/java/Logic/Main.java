16784f032acacb3c30b9702ff5b3f5d098594353
joalvarado27 <146464326+joalvarado27@users.noreply.github.com>
2023-10-03 17:49:59 -0500

    Main.java

--- src/main/java/Logic/Main.java
+++ src/main/java/Logic/Main.java
@@ -2,7 +2,9 @@ package Logic;
 
 public class Main {
     public static void main(String[] args) {
-        Notification notification = new Notification(2,1);
-        notification.sendNotification(); // Start the notification process.
+        SwingUtilities.invokeLater(() -> {
+        MainGUI gui = new MainGUI();
+        gui.setVisible(true);
+    });
     }
 }
